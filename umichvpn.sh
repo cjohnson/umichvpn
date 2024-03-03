5febcd2a3226e603ee774090eeac79b4f40f3a04
cjohnson <collin.g.johnson@gmail.com>
2024-03-03 02:16:37 -0500

    Forward the rest of the common functionality with some better aliases

--- umichvpn.sh
+++ umichvpn.sh
@@ -4,9 +4,11 @@ show_usage() {
   echo "umichvpn - A simple CLI frontend for the University of Michigan VPN."
   echo ""
   echo "Usage:"
-  echo "umichvpn connect - Connect to the VPN"
-  echo "umichvpn disconnect - Disconnect from the VPN"
-  echo "umichvpn status - Status of the VPN connection"
+  echo "umichvpn connect		Connect to the VPN"
+  echo "umichvpn disconnect		Disconnect from the VPN"
+  echo "umichvpn [status/state]		Status of the VPN connection"
+  echo "umichvpn [profiles/hosts]	Display the available VPN profiles"
+  echo "umichvpn stats			Display the VPN stats"
   echo ""
 }
 
@@ -41,8 +43,16 @@ case "$1" in
     $vpn_controller_executable disconnect
     exit_status=$?
     ;;
-  status)
+  status | state)
     $vpn_controller_executable status
+    exit_status=$?
+    ;;
+  profiles | hosts )
+    $vpn_controller_executable hosts
+    exit_status=$?
+    ;;
+  stats )
+    $vpn_controller_executable stats
     exit_status=$?
     ;;
   *)
