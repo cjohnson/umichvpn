cfce73df69d1366031386cb63952d0d74cbe667f
cjohnson <collin.g.johnson@gmail.com>
2024-03-03 01:52:57 -0500

    Add comments to new variables containing important constants
    - Added comment to explain feedforward of exit code from VPN controller.
    - Added both VPN profiles as options (TODO add functionality to switch)
    - Changed no args from -ne 1 to -eq 0 to support complex arg processing
      later on

--- umichvpn.sh
+++ umichvpn.sh
@@ -10,28 +10,45 @@ show_usage() {
   echo ""
 }
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -eq 0 ]; then
   show_usage
-  exit 1
+  exit 0
 fi
+
+# Path at which the installer places the vpn controller executable by default
+vpn_controller_executable=/opt/cisco/anyconnect/bin/vpn
+
+# The University of Michigan VPN Gateway URL
+vpn_gateway_url=umvpn.umnet.umich.edu
+
+# "UMVPN - All Traffic profile" - Includes Access to the Library Databases
+# https://its.umich.edu/enterprise/wifi-networks/vpn/getting-started
+# (see the "Notes:" section for information on the VPN profiles and their offerings)
+vpn_profile_all_traffic=umvpn-all-traffic-alt
+
+# "UMVPN - Only U-M Traffic" - Allows access to most services that require the VPN
+# https://its.umich.edu/enterprise/wifi-networks/vpn/getting-started
+# (see the "Notes:" section for information on the VPN profiles and their offerings)
+vpn_profile_only_um_traffic=umvpn-split-tunnel-alt
 
 case "$1" in
   connect)
-    /opt/cisco/anyconnect/bin/vpn connect umvpn.umnet.umich.edu/umvpn-all-traffic-alt
+    $vpn_controller_executable connect $vpn_gateway_url/$vpn_profile_only_um_traffic
     exit_status=$?
     ;;
   disconnect)
-    /opt/cisco/anyconnect/bin/vpn disconnect
+    $vpn_controller_executable disconnect
     exit_status=$?
     ;;
   status)
-    /opt/cisco/anyconnect/bin/vpn status
+    $vpn_controller_executable status
     exit_status=$?
     ;;
   *)
     show_usage
-    exit 1
+    exit 0
     ;;
 esac
 
+# Forward the exit code from the VPN controller to the exit code of the frontend.
 exit $exit_status
